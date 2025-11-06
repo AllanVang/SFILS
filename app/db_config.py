86ea424403b18a4d7c267089a976022ca52fd52d
AllanVang <Allankpvang@yahoo.com>
2025-11-06 10:30:08 -0900

    Update db_config.py

--- app/db_config.py
+++ app/db_config.py
@@ -1,1 +1,9 @@
+import mysql.connector
 
+def get_connection():
+  return mysql.connector.connector(
+    host= "127.0.0.1",
+    user="root",
+    password="Mimikyu49",
+    database="SanFranSortedDatabase"
+  )
