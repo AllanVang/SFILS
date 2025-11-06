f717f1fbc10c592c3062f0a633802dc81978dd4e
AllanVang <Allankpvang@yahoo.com>
2025-11-06 10:52:46 -0900

    Update db_config.py

--- app/db_config.py
+++ app/db_config.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 def get_connection():
-  return mysql.connector.connector(
+  return mysql.connector.connect(
     host= "127.0.0.1",
     user="root",
     password="Mimikyu49",
