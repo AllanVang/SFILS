8d3bee1d33c5483857fd1fe49d9e3bca53ae5265
AllanVang <Allankpvang@yahoo.com>
2025-11-20 22:07:34 -0900

    Update Assignment2Script.py

--- mongo/Assignment2Script.py
+++ mongo/Assignment2Script.py
@@ -21,14 +21,16 @@ db = client["SanFranSortedDatabase"]
 def transfer_table(table_name, collection_name):
     cursor.execute(f"SELECT * FROM {table_name} LIMIT 10000")
     rows = cursor.fetchall()
+    
     if rows:
         db[collection_name].insert_many(rows)
         print(f"Inserted {len(rows)} rows into {collection_name}")
 
 # Transfer each table (first 10k rows only)
-transfer_table("patron", "patron")
+transfer_table("patron", "patron") 
 transfer_table("home_library", "home_library")
 transfer_table("circulation_activity", "circulation_activity")
 transfer_table("notification_preferences", "notification_preferences")
 
+#This lets me know that Python is done importing my code
 print(" Imported first 10,000 rows into MongoDB Atlas.")
